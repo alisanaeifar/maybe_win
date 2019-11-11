f38da25f9828f513e8f379adaed5cfbe41cec8a6
ali <sanaefara@gmail.com>
2019-11-11 11:22:54 +0330

    controler/adminhtml/post/show.php
    Ç◊¸¸˛Ç

---
+++ Controller/Adminhtml/Post/Show.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Mastering\SampleModule\Controller\Adminhtml\Post;
+
+use Magento\Backend\App\Action;
+use Magento\Framework\Controller\ResultFactory;
+
+class Show extends Action
+{
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
+        $result->setContents('display');
+        return $result;
+    }
+}
