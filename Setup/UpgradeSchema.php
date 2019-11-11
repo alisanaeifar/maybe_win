245ca5588f4c9eb2c21d80519a075352640b5f62
ali <sanaefara@gmail.com>
2019-11-11 11:42:09 +0330

    Setup/Upgradeschema.php

---
+++ Setup/UpgradeSchema.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Mastering\SampleModule\Setup;
+
+use Magento\Framework\DB\Ddl\Table;
+use Magento\Framework\Setup\UpgradeSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+
+class UpgradeSchema implements UpgradeSchemaInterface
+{
+    /**
+     * @param SchemaSetupInterface $setup
+     * @param ModuleContextInterface $context
+     */
+    public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        if (version_compare($context->getVersion(), '1.0.2', '<')){
+
+            $setup->getConnection()->addColumn(
+                $setup->getTable('mastering_sample_item'),
+                'description',
+                [
+                    'type' => Table::TYPE_DATE,
+                    'nullable' => true,
+                    'comment' => 'Item Date'
+                ]
+            );
+        }
+
+        $setup->endSetup();
+    }
+}
