34f9e8184aa925467ab72a9cee8addbb402d9397
ali <sanaefara@gmail.com>
2019-11-11 14:07:43 +0330

    deleted comments from source

--- Controller/Adminhtml/Post/Config.php
+++ Controller/Adminhtml/Post/Config.php
@@ -1,22 +1,4 @@
 <?php
-//
-//namespace Mastering\SampleModule\Controller\Adminhtml\Post;
-//
-//use Magento\Framework\Controller\ResultFactory;
-//
-//class Show extends \Magento\Backend\App\Action
-//{
-//    public function execute()
-//    {
-//        /** @var \Magento\Framework\Controller\Result\Raw  $result */
-//        $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
-//        $result->setContents('display');
-//        return $result;
-//
-//
-//    }
-//}
-//
 namespace Mastering\SampleModule\Controller\Adminhtml\Post;
 
 use Magento\Backend\App\Action;
